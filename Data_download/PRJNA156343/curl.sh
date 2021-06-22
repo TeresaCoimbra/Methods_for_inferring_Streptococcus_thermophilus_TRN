d55f783c0edb8a5d553144be215c04a67ba606db
TeresaCoimbra pg42883 <55380903+TeresaCoimbra@users.noreply.github.com>
2021-06-22 18:33:06 +0100

    Add files via upload

---
+++ Data_download/PRJNA156343/curl.sh
@@ -0,0 +1,4 @@
+curl -O ftp.sra.ebi.ac.uk/vol1/fastq/SRR390/SRR390316/SRR390316_1.fastq.gz
+curl -O ftp.sra.ebi.ac.uk/vol1/fastq/SRR390/SRR390316/SRR390316_2.fastq.gz
+curl -O ftp.sra.ebi.ac.uk/vol1/fastq/SRR390/SRR390317/SRR390317_1.fastq.gz
+curl -O ftp.sra.ebi.ac.uk/vol1/fastq/SRR390/SRR390317/SRR390317_2.fastq.gz
